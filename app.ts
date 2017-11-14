2b7b6148853e39110c84619f0c73ae83d73a3c15
supuncodes <supuncodes@gmail.com>
2017-11-14 13:40:20 +0530

    New Refactorings

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
+import {TestExecutor} from "steroidslibrary/TestExecutor"
 declare function require(module:string):any;
+declare let __dirname:string;
+declare let process:any;
 
-let core = require("steroidsruntime");
-core.loadServerless();
-core.start();
+if (process.argv[2] == "test"){
+    TestExecutor.execute(__dirname);
+}else{
+    let core = require("steroidsruntime");
+    core.loadServerless();
+    core.start();
+}
